2120c40f4213f87e127f57350229f2b5913aa19b
asanaliserikuly1-ui <asanaliserikuly1@gmail.com>
2026-05-31 01:33:55 +0500

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key-change-me")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    ADMIN_USER = os.getenv("ADMIN_USER", "admin")
-    ADMIN_PASS = os.getenv("ADMIN_PASS", "admin")
+    ADMIN_USER = os.getenv("ADMIN_USER", "Kayrat_2085@mail.ru")
+    ADMIN_PASS = os.getenv("ADMIN_PASS", "autoschool2026@")
