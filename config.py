58e1b8d6cd29b65dff79ea4550c3b566e92a0b8f
asanaliserikuly1-ui <asanaliserikuly1@gmail.com>
2026-05-31 03:46:56 +0500

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,6 @@ class Config:
 
     ADMIN_USER = os.getenv("ADMIN_USER", "Kayrat_2085@mail.ru")
     ADMIN_PASS = os.getenv("ADMIN_PASS", "autoschool2026@")
+
+    os.environ["DATABASE_URL"] = "postgresql://postgres.ivykupcbocmlklkngvtn:Autoschool2026StrongPass@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres"
+    os.environ["CLOUDINARY_URL"] = "cloudinary://758634731699414:Y0BiXtXcDkyzk1md096FgAjYhlQ@dksgnnnwy"
